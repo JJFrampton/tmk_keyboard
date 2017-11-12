62e00c39d19ab626af3e44ce971e5c22c32b2754
Joseph Frampton <josephjamesframpton@gmail.com>
2017-11-12 11:24:38 -0700

    master - comments for using auto mods on a layer
    JJF

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -18,3 +18,12 @@ const action_t PROGMEM fn_actions[] = {
 // 	G,			H,			J,			K,			L,			Z,			X,			C,			V,			B,			N,			M,			Q,			W,\
 // 	E,			R,			T,			Y,			U,			I,			O,			P,			A,			S,			D,			F,			Q,			W,\
 // 	E,			R,			T,			Y,			U,			I,			O,			P,			A,			S,			D,			F,			Q,			W)
+
+// setting up automatic modifiers on a layer
+// 3.5 Momentary switching with Modifiers
+//
+// This registers modifier key(s) simultaneously with layer switching.
+//
+// ACTION_LAYER_MODS(2, MOD_LSFT | MOD_LALT)
+//
+// You can combine four modifiers at most but cannot use both left and right modifiers at a time, either left or right modiiers only can be allowed.
