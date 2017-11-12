9a883836c7b676e295bfc9f0d095446aef06eb81
Joseph Frampton <josephjamesframpton@gmail.com>
2017-11-12 11:22:00 -0700

    master - changes to base layer for linux
    JJF

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -1,11 +1,19 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	KEYMAP(TAB,  	Q,   	W,      E,      R,      T,      Y,      U,      I,      O,      P,      LBRC,   RBRC,   BSPC,      \
-           ESC, 	A,   	S,      D,      F,      G,      H,      J,      K,      L,      SCLN,   QUOT,   ENT,            \
-           LSFT, 	Z,   	X,      C,      V,      B,      N,      M,      COMM,   DOT,    SLSH,   RSFT,   FN0,          \
-           LCTL,	LALT,	FN1,                    SPC,                                    PAUS,   RALT),
+	KEYMAP(
+		GRV,			TAB,			Q,			W,			E,			R,			T,			Y,			U,			I,			O,			P,			BSLS,			BSPC,\
+		CAPS,			ESC,			A,			S,			D,			F,			G,			H,			J,			K,			L,			SCLN,		QUOT,			ENT,\
+		LSFT,			LSFT,			Z,			X,			C,			V,			B,			N,			M,			COMM,		DOT,		SLSH,		RSFT,			FN0,\
+		FN1,			LCTL,			LGUI,		LALT,		FN2,		FN3,		FN4,		SPC,		SPC,		RGUI,		FN5,		FN6,		FN7,			FN8),
 };
 const action_t PROGMEM fn_actions[] = {
 
 };
+
+// could be used for testing:
+// KEYMAP(
+// 	Q,			W,			E,			R,			T,			Y,			U,			I,			O,			P,			A,			S,			D,			F,\
+// 	G,			H,			J,			K,			L,			Z,			X,			C,			V,			B,			N,			M,			Q,			W,\
+// 	E,			R,			T,			Y,			U,			I,			O,			P,			A,			S,			D,			F,			Q,			W,\
+// 	E,			R,			T,			Y,			U,			I,			O,			P,			A,			S,			D,			F,			Q,			W)
